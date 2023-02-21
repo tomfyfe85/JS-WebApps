5deadf3ce335a984df9d1a86de5c4e633c19c604
Tom Fyfe <tomfyfe85@gmail.com>
2023-02-21 02:55:08 +0000

    part 2

--- notesApp_phase1/index.js
+++ notesApp_phase1/index.js
@@ -3,6 +3,10 @@
 // to help in debugging and getting visibility in your JS code.
 //
 // on Mac (using Chrome), use Option+Command+J to open the console and see this message.
+const add = require('./add')
+const multiply = require('./multiply')
 
 console.log("Hello from the developer console!");
 console.log(new Date());
+
+console.log(multiply(add(2, 2), 4));
