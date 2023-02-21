25795371581c4a25f69e9cad79a2b618a0e9327f
Tom Fyfe <tomfyfe85@gmail.com>
2023-02-21 16:35:00 +0000

    part5 complete

--- example2/view.js
+++ example2/view.js
@@ -1,8 +1,15 @@
 class View {
   constructor() {
-    this.mainContainerEl = document.querySelector('#main-container');
+    this.mainContainerEl = document.querySelector("#main-container");
 
     console.log(this.mainContainerEl);
+  }
+
+  addParagraph() {
+    const newParagraph = document.createElement("p");
+    newParagraph.textContent =
+      "This paragraph has been dynamically added by JavaScript!";
+    this.mainContainerEl.append(newParagraph);
   }
 }
 
