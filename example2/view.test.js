bee4c5e3aac17059ae8dff39e557a6b4653398fa
Tom Fyfe <tomfyfe85@gmail.com>
2023-02-21 17:04:14 +0000

    added test for addParagraph() - passes

--- example2/view.test.js
+++ example2/view.test.js
@@ -2,15 +2,22 @@
  * @jest-environment jsdom
  */
 
-const fs = require('fs');
-const View = require('./view');
+const fs = require("fs");
+const View = require("./view");
 
-describe('Page view', () => {
-  it('displays 2 paragraphs', () => {
-    document.body.innerHTML = fs.readFileSync('./index.html');
+describe("Page view", () => {
+  beforeEach(() => {
+    document.body.innerHTML = fs.readFileSync("./index.html");
+  });
 
+  it("displays 2 paragraphs", () => {
     const view = new View();
+    expect(document.querySelectorAll("p").length).toBe(2);
+  });
 
-    expect(document.querySelectorAll('p').length).toBe(2);
+  it("displays 3 paragraphs after addParagraph() has be called", () => {
+    const view = new View();
+    view.addParagraph();
+    expect(document.querySelectorAll("p").length).toBe(3);
   });
 });
