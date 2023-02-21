3bb52e5c930d435955fc99d7b8157ace591e9e55
Tom Fyfe <tomfyfe85@gmail.com>
2023-02-21 02:29:30 +0000

    part 1

--- notesApp_phase1/index.js
+++ notesApp_phase1/index.js
@@ -4,4 +4,5 @@
 //
 // on Mac (using Chrome), use Option+Command+J to open the console and see this message.
 
-console.log('Hello from the developer console!');
+console.log("Hello from the developer console!");
+console.log(new Date());
