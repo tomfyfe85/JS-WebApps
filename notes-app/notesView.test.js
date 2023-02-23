cf11427fcf9ea6ec444f0e19463c21c260ab3e6d
Tom Fyfe <tomfyfe85@gmail.com>
2023-02-23 21:52:05 +0000

    created test for notesView class

--- notes-app/notesView.test.js
+++ notes-app/notesView.test.js
@@ -5,17 +5,25 @@
 // will have a method 'displayNotes()' which gets list of notes from
 // notesModel and will create a new div element on the page (with an HTML class 'note')
 
-// need to require notesModel and notesModel.addNote('go to bed') 
-// 
+// need to require notesModel and notesModel.addNote('go to bed') ?
+//
+const fs = require("fs");
+const NotesView = require('./notesView')
+const NotesModel = require('./notesModel')
 
-Describe("NotesView", () => {
+describe("NotesView", () => {
   beforeEach(() => {
     document.body.innerHTML = fs.readFileSync("./index.html");
   });
-   
-    it("returns number of divs with class name 'note'", () => {
-        const notesView = new NotesView()
-        // notesView.displayNotes(model.getNotes)
-        expect(document.getElementsByClassName('note').length && document.querySelectorAll('div').length ).toBe(1)
-    })
+
+  it("returns number of divs with class name 'note'", () => {
+     const notesModel = new NotesModel
+     const notesView = new NotesView(notesModel);
+      notesModel.addNote("i'm a note!")
+      notesView.displayNotes();
+    expect(
+      document.getElementsByClassName("note").length &&
+        document.querySelectorAll("div").length
+    ).toBe(1);
+  });
 });
