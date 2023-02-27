bb071bb71443eebd914ac87ad845fb7120211070
Tom Fyfe <tomfyfe85@gmail.com>
2023-02-27 22:22:04 +0000

    exercise 1 completed

--- example3/messageView.js
+++ example3/messageView.js
@@ -1,14 +1,18 @@
 class MessageView {
   constructor() {
-    this.buttonEl = document.querySelector('#show-message-button');
-
-    this.buttonEl.addEventListener('click', () => {
-       this.displayMessage();
+    this.buttonEl = document.querySelector("#show-message-button");
+    this.mainContainerEl = document.querySelector("#main-container");
+    this.buttonEl.addEventListener("click", () => {
+      this.displayMessage();
     });
   }
 
   displayMessage() {
-    console.log('Thanks for clicking me!');
+    const newMessage = document.createElement("div");
+    newMessage.setAttribute("id", "message");
+    newMessage.innerText = "This message is displayed by JavaScript";
+    this.mainContainerEl.append(newMessage);
+    console.log("Thanks for clicking me!");
   }
 }
 
