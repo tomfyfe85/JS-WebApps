2686892e8c9a0b3ff922cca1621dc40c0df52310
Tom Fyfe <tomfyfe85@gmail.com>
2023-03-02 12:48:36 +0000

    added testing notes to scratch.txt

--- notes-app/index.js
+++ notes-app/index.js
@@ -2,7 +2,6 @@ const NotesModel = require("./notesModel");
 const NotesView = require("./notesView1");
 
 const model = new NotesModel();
-model.addNote("This is an example note");
-model.addNote("This is an example note2");
+
 const view = new NotesView(model);
 view.displayNotes();
