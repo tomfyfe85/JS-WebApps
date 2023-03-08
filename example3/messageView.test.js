e68aed0c6a302b997bd4cdf996e7b3dfd63bc401
Tom Fyfe <tomfyfe85@gmail.com>
2023-03-08 21:47:34 +0000

    resolved merge conflicts

--- example3/messageView.test.js
+++ example3/messageView.test.js
@@ -1,19 +1,32 @@
-/**
- * @jest-environment jsdom
- */
+const fs = require("fs");
+const MessageView = require("./messageView");
 
-const fs = require('fs');
-const MessageView = require('./messageView');
+describe("MessageView", () => {
+  it("clicks the button", () => {
+    document.body.innerHTML = fs.readFileSync("./index.html");
 
-describe('MessageView', () => {
-  it('clicks the button', () => {
-    document.body.innerHTML = fs.readFileSync('./index.html');
+    const view = new MessageView();
+    const inputEl = document.querySelector("#message-input");
+    inputEl.value = "BEER ME!";
+   
+    const buttonEl = document.querySelector("#show-message-button");
+    buttonEl.click();
+
+    expect(document.querySelector("#message")).not.toBeNull();
+    expect(document.querySelector("div#message").innerText).toBe("BEER ME!");
+  });
+
+  it("clicks the hide button", () => {
+    document.body.innerHTML = fs.readFileSync("./index.html");
 
     const view = new MessageView();
 
-    const buttonEl = document.querySelector('#show-message-button');
+    const buttonEl = document.querySelector("#show-message-button");
     buttonEl.click();
 
-    expect(document.querySelector('#message')).not.toBeNull();
+    const hideEl = document.querySelector("#hide-message-button");
+    hideEl.click();
+
+    expect(document.querySelector("#message")).toBeNull();
   });
 });
