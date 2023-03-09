67d9fd579d5062469073a570e7491149fce9062c
Tom Fyfe <tomfyfe85@gmail.com>
2023-03-09 19:13:26 +0000

    refactored notesView

--- notes-app/notesView.js
+++ notes-app/notesView.js
@@ -3,22 +3,17 @@ class NotesView {
     this.model = model;
     this.mainContainerEl = document.querySelector("#main-container");
     this.newDiv = "";
-    this.addNoteButtonEl = document.querySelector("#add-note-button");
-
-    this.addNoteButtonEl.addEventListener("click", () => {
+   
+    document.querySelector("#add-note-button").addEventListener("click", () => {
       this.displayNotes();
-      console.log("button clicked");
     });
   }
 
   displayNotes() {
-    console.log("displayNotes");
-
     const inputEl = document.querySelector("#note-input");
     this.model.addNote(inputEl.value);
     const notes = this.model.getNotes();
 
-    // const newDiv = document.createElement("div");
     notes.forEach((note) => {
       this.newDiv = document.createElement("div");
       this.newDiv.classList.add("note");
