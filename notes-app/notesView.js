383cfd910210a45e3568d28bc6e02d31d60f3a70
Tom Fyfe <tomfyfe85@gmail.com>
2023-03-09 19:01:49 +0000

    part 10 complete

--- notes-app/notesView.js
+++ notes-app/notesView.js
@@ -3,15 +3,21 @@ class NotesView {
     this.model = model;
     this.mainContainerEl = document.querySelector("#main-container");
     this.newDiv = "";
-    this.addNoteButtonEl = document.querySelector("#note-input");
+    this.addNoteButtonEl = document.querySelector("#add-note-button");
 
     this.addNoteButtonEl.addEventListener("click", () => {
+      this.displayNotes();
       console.log("button clicked");
     });
   }
 
   displayNotes() {
+    console.log("displayNotes");
+
+    const inputEl = document.querySelector("#note-input");
+    this.model.addNote(inputEl.value);
     const notes = this.model.getNotes();
+
     // const newDiv = document.createElement("div");
     notes.forEach((note) => {
       this.newDiv = document.createElement("div");
@@ -19,6 +25,7 @@ class NotesView {
       this.newDiv.innerText = note;
       this.mainContainerEl.append(this.newDiv);
     });
+    inputEl.value = "";
   }
 }
 
